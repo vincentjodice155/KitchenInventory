456863fed5fecb3da880304d9d5e7ce2fef00906
jodicev <jodicev>
2021-06-30 21:22:00 -0400

    started to implement the dropdown menu, ran into a hurdle trying to connect name variable in registerUser and dropdown value

--- app/src/main/java/testing/example/kitchen/AddChocolateList.java
+++ app/src/main/java/testing/example/kitchen/AddChocolateList.java
@@ -6,8 +6,11 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
@@ -21,15 +24,18 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-public class AddChocolateList extends AppCompatActivity implements View.OnClickListener {
+public class AddChocolateList extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
     private EditText editTextName, editTextAlert, editTextQuantity,editTextPopular;
 
     private Button buttonDatabaseAdd;
     private Button backButton;
     private ProgressDialog progressDialog;
+
+    private Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +45,19 @@ public class AddChocolateList extends AppCompatActivity implements View.OnClickL
         editTextAlert = (EditText) findViewById(R.id.alert_input);
         editTextQuantity = (EditText) findViewById(R.id.quantity_input);
         editTextPopular = (EditText) findViewById(R.id.popular_input);
+
+        //creating Spinner Stuff
+        spinner = findViewById(R.id.spinner);
+
+        ArrayList<String> chocolateTypes = new ArrayList<>();
+        chocolateTypes.add("Milk Chocolate");
+        chocolateTypes.add("Dark Chocolate");
+        chocolateTypes.add("White Chocolate");
+
+        ArrayAdapter spinnerAdapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item,chocolateTypes);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(spinnerAdapter);
+        spinner.setOnItemSelectedListener(this);
 
         buttonDatabaseAdd = (Button) findViewById(R.id.buttonAdd);
 
@@ -61,6 +80,7 @@ public class AddChocolateList extends AppCompatActivity implements View.OnClickL
     }
 
     private void registerUser(){
+
         final String name = editTextName.getText().toString().trim();
         final String alert = editTextAlert.getText().toString().trim();
         final String quantity = editTextQuantity.getText().toString().trim();
@@ -107,10 +127,26 @@ public class AddChocolateList extends AppCompatActivity implements View.OnClickL
         requestQueue.add(stringRequest);
 
     }
+
+
+    //Method for Clicking button allowing entry of name into database
     @Override
     public void onClick(View v) {
         //when you click the button it activated the registerUser method
         if(v == buttonDatabaseAdd)
             registerUser();
+    }
+
+    //Methods for Spinner
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String chocolateType = parent.getItemAtPosition(position).toString();
+        String newChocolateTyoe = chocolateType + " " + editTextName.getText().toString();
+        Toast.makeText(getApplicationContext(),chocolateType,Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
